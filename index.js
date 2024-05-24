9376f9b9b02cd58c8748ecbd99868a91c27c85db
Jérémy Tarlié <contact@tarlie.fr>
2024-05-24 09:50:52 +0200

    first commit

--- index.js
+++ index.js
@@ -11,53 +11,41 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 
-// Root route serving the navigation page
 app.get('/', (req, res) => {
   res.render('index');
 });
 
-// Route to render the user page with forms
 app.get('/createUser', (req, res) => {
   res.render('user');
 });
 
-// Route to render the get user page with forms
 app.get('/getUser', async (req, res) => {
-  // Retrieve all users from SQL database
   const users = await db.User.findAll();
 
-  // Render the getUser page and pass the users data to the template
   res.render('getUser', { users });
 });
 
 
-// Handle form submissions for creating a user
 app.post('/user', async (req, res) => {
   const { name, email } = req.body;
 
-  // Save user to SQL database
   const createdUser = await db.User.create({ name, email });
 
-  // Save user to Redis
   await setUser(createdUser);
 
   res.status(201).json(createdUser);
 });
 
-// Handle form submissions for retrieving a user
 app.get('/user', async (req, res) => {
   const { id } = req.query;
 
-  // Retrieve user from Redis
   const user = await getUser(id);
 
   if (user) {
     res.json(user);
   } else {
-    // Retrieve user from SQL database if not found in Redis
     const userFromDb = await db.User.findByPk(id);
     if (userFromDb) {
-      // Save retrieved user to Redis for future requests
       await setUser(userFromDb);
       res.json(userFromDb);
     } else {
